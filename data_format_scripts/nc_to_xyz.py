76e96634660a7a8f197bd95337af528449d5e7d6
mahmoud-al-najar <mahmoud.najar.95@gmail.com>
2020-12-21 16:03:45 +0100

    added read_nc_file to common utils + added data format scripts dir

---
+++ data_format_scripts/nc_to_xyz.py
@@ -0,0 +1,35 @@
+import os
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+from utilities.common import read_nc_file
+
+
+fn_dir = '/media/mn/WD4TB/PEPS/data/pbm_results/22NCL'
+dir_out = '/media/mn/WD4TB/PEPS/data/pbm_results/22NCL'
+out_headers = ['lng', 'lat', 'z']
+show = True
+save_fig = True
+for fn in os.listdir(fn_dir):
+    if fn.endswith('.nc'):
+        fn_path = os.path.join(fn_dir, fn)
+        lng, lat, z = read_nc_file(fn_path)
+
+        if show or save_fig:
+            plt.figure(figsize=(8, 5))
+            plt.rcParams.update({'font.size': 12})
+            np.set_printoptions(suppress=True)
+            plt.scatter(lng, lat, c=z, cmap='ocean_r', vmax=50, marker=',', s=np.full(len(lng), 1))
+            plt.colorbar(shrink=0.5)
+            plt.tight_layout(pad=2)
+            plt.suptitle(f'{fn.replace(".nc", "")}')
+            if show:
+                plt.show()
+            if save_fig:
+                plt.savefig(os.path.join(dir_out, f'{fn}.png'))
+
+        with open(os.path.join(dir_out, f'{fn}.xyz'), 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(out_headers)
+            writer.writerows(zip(lng, lat, z))
+            print(f'    out_{fn}.xyz')
--- utilities/common.py
+++ utilities/common.py
@@ -2,9 +2,10 @@ import numpy as np
 import model_training.config as cfg
 import json
 import os
+import netCDF4 as nc
 
 
-def flip_array(arr):
+def __flip_bathymetry_y_axis(arr):
     unique_values = np.unique(arr)
     flipped = np.empty(np.array(arr).shape)
     for i in range(len(arr)):
@@ -47,3 +48,56 @@ def make_training_log_file():
 
 def get_nan_mean(preds):
     return np.nanmean(np.dstack(preds), axis=2)
+
+
+def read_nc_file(path_to_nc, projection_in=None, projection_out=None):
+    ncd = nc.Dataset(path_to_nc)
+    print(ncd)
+
+    n_x = len(ncd.variables['x'])
+    n_y = len(ncd.variables['y'])
+    n_k = len(ncd.variables['kKeep'])
+    n_t = len(ncd.variables['time'])
+
+    out_x = []
+    out_y = []
+    out_z = []
+    n_err = 0
+    n_good = 0
+    n_all = 0
+    n_dash = 0
+
+    for i_t in range(n_t):
+        for i_x in range(n_x):
+            for i_y in range(n_y):
+                ncd_time = ncd.variables['time'][i_t]
+                ncd_x = ncd.variables['x'][i_x]
+                ncd_y = ncd.variables['y'][i_y]
+                z = None
+                for i_k in range(n_k):
+                    ncd_z = ncd['depth'][i_y, i_x, i_k, i_t]
+                    n_all += 1
+
+                    if ncd_z != '--':
+                        if z is None:
+                            z = ncd_z
+                            n_good += 1
+                            out_x.append(ncd_x)
+                            out_y.append(ncd_y)
+                            out_z.append(z)
+                        else:
+                            new_z = (z + ncd_z) / 2
+                            z = new_z
+                            n_err += 1
+                    else:
+                        n_dash += 1
+                if n_all % 5000 == 0:
+                    print(f'all: {n_all}, keep: {n_good}, errs: {n_err}, dash: {n_dash}')
+    fn = path_to_nc.split("/")[-1]
+    print(f'Filename: {fn}')
+    print(f'    Total: {n_all}, 1k: {n_good}, nk: {n_err}, --: {n_dash}')
+    print(f'    len(x): {len(out_x)}, len(y): {len(out_y)}, len(z): {len(out_z)}')
+
+    print(f'    Creating CSV file for {fn}...')
+    out_y = __flip_bathymetry_y_axis(out_y)
+    return out_x, out_y, out_z
