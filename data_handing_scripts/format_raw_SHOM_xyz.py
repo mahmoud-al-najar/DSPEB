5193dc19923d30888280f807eaf3f3c23de98623
mahmoud-al-najar <mahmoud.najar.95@gmail.com>
2020-12-22 20:16:21 +0100

    bugfix format_raw_SHOM_xyz.py

--- data_handing_scripts/format_raw_SHOM_xyz.py
+++ data_handing_scripts/format_raw_SHOM_xyz.py
@@ -12,7 +12,6 @@ path_raw_bathy = 'path/to/raw/xyz'
 df = pd.read_csv(path_raw_bathy, header=2)
 df = df.rename(columns={'long(DD)': 'lng', 'lat(DD)': 'lat', 'depth(m - down positive - LAT)': 'z'})
 
-df = df.sample(10000)
 x, y, epsg = get_top_left_corner_coordinates_for_image(path_example_safe)
 if epsg != 'EPSG:4326':
     proj = pyproj.Proj(proj='utm', init=epsg, ellps='WGS84')
